dd72efae488dd34a23edf0dc9a7ea5713401a9dc
nskselvam <47290672+nskselvam@users.noreply.github.com>
2023-02-08 14:38:15 +0530

    'f3'

--- food/views.py
+++ food/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import food_table
-
 # Create your views here.
-
 def index(request):
     Item_data=food_table.objects.all()
     Context={
